8c0b6d309efd888379ae6e1a308165da1e8995eb
Fernando Ribeiro <fernando.augusto@hotmail.com.br>
2023-08-21 22:02:14 -0300

    acertado para aguardar o tempo de provisionamento do banco

--- sql/import-data.sh
+++ sql/import-data.sh
@@ -1,4 +1,6 @@
 #aguardando 90 segundos para aguardar o provisionamento e start do banco
+echo "Aguardando 90 segundos..."
 sleep 90s
+echo "Continuando com os passos após o provisionamento e start do banco"
 #rodar o comando para criar o banco
 /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U SA -P "Mudar123intrA" -i criacao-banco-docker.sql
