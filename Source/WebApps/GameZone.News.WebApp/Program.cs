77f06fe7b75c7f4e18c33d5162ecf4ccb6166bb0
Fernando Ribeiro <fernando.augusto@hotmail.com.br>
2023-08-30 22:45:23 -0300

    teste

--- Source/WebApps/GameZone.News.WebApp/Program.cs
+++ Source/WebApps/GameZone.News.WebApp/Program.cs
@@ -1,45 +1,39 @@
 using GameZone.News.WebApp.Configurations;
 
-class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+ConfigureServices.Configure(builder);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        ConfigureServices.Configure(builder);
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        }
-        app.UseExceptionHandler("/erro/500");
-        app.UseStatusCodePagesWithRedirects("/erro/{0}");
-
-        app.UseForwardedHeaders();
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        //app.UseMiddleware<ExceptionMiddleware>();
-        app.UseSession();
-
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-        });
-
-        app.Run();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 }
+app.UseExceptionHandler("/erro/500");
+app.UseStatusCodePagesWithRedirects("/erro/{0}");
+
+app.UseForwardedHeaders();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+//app.UseMiddleware<ExceptionMiddleware>();
+app.UseSession();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
