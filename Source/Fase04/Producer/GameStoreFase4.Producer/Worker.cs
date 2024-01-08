5b984023ae02c61db165d796032b7dbfeec0faf8
andreleaos <andreleaos@gmail.com>
2024-01-07 22:06:07 -0300

    Implementacao classe Worker

--- Source/Fase04/Producer/GameStoreFase4.Producer/Worker.cs
+++ Source/Fase04/Producer/GameStoreFase4.Producer/Worker.cs
@@ -1,20 +1,41 @@
+using GameStoreFase4.Domain.Entities;
+using GameStoreFase4.Services.Generator;
+using GameStoreFase4.Services.Messages.Producer;
+using Newtonsoft.Json;
+
 namespace GameStoreFase4.Producer
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IGeneratorDataService _generatorDataService;
+        private readonly IProducerRabbitMqService _producerRabbitMqService;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IGeneratorDataService generatorDataService, IProducerRabbitMqService producerRabbitMqService)
         {
             _logger = logger;
+            _generatorDataService = generatorDataService;
+            _producerRabbitMqService = producerRabbitMqService;
         }
+
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                _logger.LogInformation("\nWorker running at: {time}", DateTimeOffset.Now);
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Jogo jogo = _generatorDataService.Generate();
+                    var message = JsonConvert.SerializeObject(jogo);
+                    await _producerRabbitMqService.Publish(message, jogo.GetType(), publishDlq: false);
+
+                    var logMessage = $"Jogo [{jogo.Nome.ToUpper()}] publicado na fila\n";
+                    _logger.LogInformation(logMessage);
+                }
+
+                await Task.Delay(5000, stoppingToken);
             }
         }
     }
